b04c3c860ba8f6c9d1770bf3d1ea6512f647e003
MagicExist <johhanparra1@gmail.com>
2026-01-21 17:05:54 -0500

    feat: add UserChallenge pivot model

--- back-end/core/models.py
+++ back-end/core/models.py
@@ -35,6 +35,11 @@ class User(AbstractBaseUser, PermissionsMixin):
         
     )
     streak = models.PositiveIntegerField(default=0)
+    challenges = models.ManyToManyField(
+        "Challenge",
+        through='UserChallenge',
+        related_name='users'
+    )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -102,3 +107,10 @@ class Achievement(models.Model):
     def __str__(self):
         return self.title
 
+class UserChallenge(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    complete_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.challenge.title}"
