d3e3d4cc02ffb0760e64a131ca2a9be3b3443552
MagicExist <johhanparra1@gmail.com>
2026-01-22 13:39:15 -0500

    feat: add achievement basic viewset

--- back-end/core/views.py
+++ back-end/core/views.py
@@ -5,3 +5,9 @@ from .serializers import AchievementSerializer,ChallengeSerializer
 class ChallengeViewSet(ModelViewSet):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
+
+
+class AchievementViewSet(ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
+
