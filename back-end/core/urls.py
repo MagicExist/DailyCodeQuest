9fcdaa9c7cde64e8c1dcd48e7fc9ec04c6d47270
MagicExist <johhanparra1@gmail.com>
2026-01-22 19:35:01 -0500

    feat: add user urls

--- back-end/core/urls.py
+++ back-end/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import ChallengeViewSet,AchievementViewSet
+from .views import ChallengeViewSet,AchievementViewSet,UserViewSet
 
 router = DefaultRouter()
 router.register(r'challenges', ChallengeViewSet , basename='challenge')
 router.register(r'achievements',AchievementViewSet,basename='achievement')
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    path('users/',UserViewSet.as_view(),name='users')
 ]
