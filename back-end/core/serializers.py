f26ea9f5b5522be05a9b445a89358dd0ecd41a29
MagicExist <johhanparra1@gmail.com>
2026-01-22 19:28:27 -0500

    feat: add user serializer

--- back-end/core/serializers.py
+++ back-end/core/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
-from .models import Challenge,Achievement
+from .models import Challenge,Achievement,User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email','password','username','streak']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
 
 class ChallengeSerializer(serializers.Serializer):
     class Meta:
