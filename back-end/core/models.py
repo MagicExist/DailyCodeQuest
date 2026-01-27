763b243711a47b06dded0a0e9e63722bd06931d9
MagicExist <johhanparra1@gmail.com>
2026-01-27 14:05:28 -0500

    feat: add ShuffleChallenge model

--- back-end/core/models.py
+++ back-end/core/models.py
@@ -127,3 +127,9 @@ class UserAchievement(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.achievement.title}"
+
+class ShuffleChallenge(models.Model):
+    original_list = models.JSONField(default=list)
+    dynamic_list = models.JSONField(default=list)
+    updated_at = models.DateTimeField(auto_now=True)
+
