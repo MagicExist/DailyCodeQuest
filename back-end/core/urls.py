0eac6fc010acd3b845fa922feab374f7d16bafa5
MagicExist <johhanparra1@gmail.com>
2026-03-05 15:10:26 -0500

    feat: add daily challenge url

--- back-end/core/urls.py
+++ back-end/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import ChallengeViewSet, AchievementViewSet, UserViewSet, TagViewSet
+from .views import ChallengeViewSet, AchievementViewSet, UserViewSet, TagViewSet, DailyChallengeView
 
 router = DefaultRouter()
 router.register(r'challenges', ChallengeViewSet , basename='challenge')
@@ -9,5 +9,6 @@ router.register(r'tags', TagViewSet, basename='tag')
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('users/',UserViewSet.as_view(),name='users')
+    path('users/',UserViewSet.as_view(),name='users'),
+    path('daily_challenge/',DailyChallengeView.as_view(),name="daily_challenge")
 ]
