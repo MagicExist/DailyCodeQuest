d54fae07a1889cc2837a33ce984c36c0bffc76f0
MagicExist <johhanparra1@gmail.com>
2026-03-05 15:25:42 -0500

    feat: add achievement to admin panel

--- back-end/core/admin.py
+++ back-end/core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Challenge, DailyChallenge, ShuffleChallenge, Tag
+from .models import Challenge, DailyChallenge, ShuffleChallenge, Tag, Achievement
 
 # Register your models here.
 admin.site.register(Challenge)
 admin.site.register(DailyChallenge)
 admin.site.register(ShuffleChallenge)
 admin.site.register(Tag)
+admin.site.register(Achievement)
